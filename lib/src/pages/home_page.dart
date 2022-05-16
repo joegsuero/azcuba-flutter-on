62f225b92819cce500037a70c4c59f6bf23232b0
FlutLab.io <>
2022-05-16 19:39:22 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -14,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SlidingUpPanel(
-        controller: _pc,
+        controller: _pc.close(),
         panel: Center(
           child: Text("This is the sliding Widget"),
         ),
