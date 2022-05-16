dec17ee101d5a0c3fc66e567bef2a51d431ab074
FlutLab.io <>
2022-05-16 22:15:05 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -45,8 +45,8 @@ Widget _body() {
               MarkerLayerOptions(
                 markers: [
                   Marker(
-                    width: 800.0,
-                    height: 800.0,
+                    width: 8000.0,
+                    height: 8000.0,
                     point: LatLng(21.463, -78.322),
                     builder: (ctx) => Container(
                       child: FlutterLogo(),
