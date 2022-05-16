d80ce28a1f0365be0dd229b4903f86eaadccee34
FlutLab.io <>
2022-05-16 20:31:55 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -18,7 +18,7 @@ class _HomePageState extends State<HomePage> {
         panel: Center(
           child: Text("This is the sliding Widget"),
         ),
-        onPanelClosed: True,
+        onPanelClosed: null,
         body: Center(
           child: Container(
             child: Column(
