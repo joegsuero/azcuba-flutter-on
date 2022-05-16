05c80137d3fe1c468304559e27bafe4b0f71f980
FlutLab.io <>
2022-05-16 18:19:05 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+//
+import 'package:application/src/pages/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
     home: MyApp(),
   ));
-}
-
-class MyApp extends StatefulWidget {
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          child: Column(
-            children: [
-              Flexible(
-                child: FlutterMap(
-                  options: MapOptions(center: LatLng(21.463, -78.322), zoom: 6),
-                  layers: [
-                    TileLayerOptions(urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", subdomains: [
-                      'a',
-                      'b',
-                      'c'
-                    ]),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
