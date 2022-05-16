168f493b2e32f8c100abf484d99e9d03ec4c21a9
FlutLab.io <>
2022-05-16 19:40:18 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -9,7 +9,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   PanelController _pc = new PanelController();
-
+  _pc.hide();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
