8d18755667e39f2de1b3c6480b2d5ea27a69bbce
FlutLab.io <>
2022-05-16 16:55:23 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
