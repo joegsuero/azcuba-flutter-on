bc34e0705bf27c1109256123676b6ddbc49db988
FlutLab.io <>
2022-05-17 20:03:28 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         primaryColor: Colors.green.shade900,
       )));
 */
-
+  WidgetsFlutterBinding.ensureInitialized();
   var empr = const Empresa(
     id: 1,
     name: "CUJAI",
