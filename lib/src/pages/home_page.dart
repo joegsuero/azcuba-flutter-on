72c3cc202e3b9ea12bc5a66e1028a25e7da6e8c5
FlutLab.io <>
2022-05-17 00:01:43 +0000

    Autosave

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -24,7 +24,7 @@ Widget _buildSlidingPanel(PanelController pc) {
     controller: pc,
     panel: Center(
       child: Text("This is the sliding Widget"),
-    ),
+    )collapsed: null,
     body: _body(pc),
   );
 }
@@ -71,7 +71,7 @@ Widget _searchButton(PanelController pc) {
       child: const Icon(Icons.search),
       backgroundColor: Colors.green.shade900,
       onPressed: () {
-        pc.hide();
+        //pc.hide();
         print("sasasas");
       });
 }
